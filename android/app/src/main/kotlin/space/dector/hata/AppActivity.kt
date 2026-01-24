ca84eabd3fbf543fe628e56f7f6092f83c0c6d06
D <dector@dector.space>
2026-01-25 00:14:24 +0100

    ref: create preview function

--- android/app/src/main/kotlin/space/dector/hata/AppActivity.kt
+++ android/app/src/main/kotlin/space/dector/hata/AppActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import space.dector.hata.ui.theme.HataTheme
 
 
@@ -39,10 +45,24 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@PreviewLightDark
 @Composable
-fun GreetingPreview() {
+fun GreetingPreview() = preview {
+    Greeting("Android")
+}
+
+@Composable
+fun preview(
+    pad: Dp = 0.dp,
+    content: @Composable () -> Unit
+) {
     HataTheme {
-        Greeting("Android")
+        Box(
+            modifier = Modifier
+                .background(MaterialTheme.colorScheme.background)
+                .padding(pad),
+        ) {
+            content()
+        }
     }
 }
